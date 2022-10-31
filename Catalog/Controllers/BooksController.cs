0ed3e868e193cafba6a987e4a735bb98f917fd63
Violetta Bayeva <violetta.bayeva@dreamstormstudios.com>
2022-10-31 19:20:40 +0100

    Provided user side of the delete, update and add functions

--- Catalog/Controllers/BooksController.cs
+++ Catalog/Controllers/BooksController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Catalog.Dtos;
 using Catalog.Entities;
 using Catalog.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -7,29 +9,92 @@ using Microsoft.AspNetCore.Mvc;
 namespace Catalog.Controllers
 {
     // GET /books
-    
+
     [ApiController]
     [Route("books")]
     public class BooksController : ControllerBase
     {
-        private readonly InMemBookRepository _repository;
+        private readonly IBookRepository _repository;
 
-        public BooksController()
+        public BooksController(IBookRepository repository)
         {
-            _repository = new ();
+            _repository = repository;
         }
 
         [HttpGet]
-        public IEnumerable<Book> GetBooks()
+        public IEnumerable<BookDto> GetBooks()
         {
-            return _repository.GetBooks();
+            return _repository.GetBooks().Select(book => book.AsDto());
         }
 
         //GET /item/{id}
         [HttpGet("{id}")]
-        public Book GetBooks(Guid id)
+        public ActionResult<BookDto> GetBook(Guid id)
         {
-            return _repository.GetBook(id);
+            var book = _repository.GetBook(id);
+
+            if (book is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book.AsDto());
+        }
+
+        // POST /books
+        [HttpPost]
+        public ActionResult<BookDto> AddBook(CreateBookDto createBookDto)
+        {
+            Book book = new()
+            {
+                Id = Guid.NewGuid(),
+                Name = createBookDto.Name,
+                Author = createBookDto.Author,
+                PageCount = createBookDto.PageCount
+            };
+
+            _repository.CreateBook(book);
+
+            return CreatedAtAction(nameof(GetBook), new {id = book.Id}, book.AsDto());
+        }
+
+        // PUT /item
+        [HttpPut("{id}")]
+        public ActionResult UpdateBook(Guid id, UpdateBookDto updateBookDto)
+        {
+            var existingBook = _repository.GetBook(id);
+
+            if (existingBook is null)
+            {
+                return NotFound();
+            }
+
+            Book updatedBook = existingBook with
+            {
+                Name = updateBookDto.Name,
+                Author = updateBookDto.Author,
+                PageCount = updateBookDto.PageCount
+            };
+            
+            _repository.UpdateBook(updatedBook);
+
+            return NoContent();
+        }
+
+        // DELETE /item/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingBook = _repository.GetBook(id);
+
+            if (existingBook is null)
+            {
+                return NotFound();
+            }
+            
+            _repository.DeleteBook(existingBook);
+
+            return NoContent();
         }
     }
 }
---
+++ Catalog/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.Dtos;
+using Catalog.Entities;
+
+namespace Catalog
+{
+    public static class Extensions
+    {
+        public static BookDto AsDto(this Book book)
+        {
+            return new BookDto() {Id = book.Id, Name = book.Name, Author = book.Author, PageCount = book.PageCount};
+        }
+    }
+}
