9b368c234ce96ca9bf7b97239d21e8a37d43bb46
Violetta Bayeva <violetta.bayeva@dreamstormstudios.com>
2022-10-31 19:37:56 +0100

    Created new repository type using mongodb

---
+++ Catalog/Repositories/MongoDbBooksRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Catalog.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Repositories
+{
+    public class MongoDbBooksRepository : IBookRepository
+    {
+        private const string DatabaseName = "catalog";
+        private const string CollectionName = "books";
+        
+        private readonly IMongoCollection<Book> _booksCollection;
+        
+        public MongoDbBooksRepository(IMongoClient mongoClient)
+        {
+            IMongoDatabase database = mongoClient.GetDatabase(DatabaseName);
+            _booksCollection = database.GetCollection<Book>(CollectionName);
+        }
+        
+        public IEnumerable<Book> GetBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book GetBook(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
