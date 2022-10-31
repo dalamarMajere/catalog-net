40365fc7d65cb27b2fd92bc487d688fdb6b13ccd
Violetta Bayeva <violetta.bayeva@dreamstormstudios.com>
2022-10-31 19:19:40 +0100

    Added abstraction to repository class

---
+++ Catalog/Repositories/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public interface IBookRepository
+    {
+        public IEnumerable<Book> GetBooks();
+
+        public Book GetBook(Guid id);
+
+        public void CreateBook(Book book);
+        public void UpdateBook(Book book);
+        public void DeleteBook(Book book);
+    }
+}
